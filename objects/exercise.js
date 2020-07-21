5b62028e4371930cbe047ce31af02a82b90a02ec
Jonathan Yale <jonathanyale@gmail.com>
2020-07-21 13:06:46 -0400

    broken implementation. See comments for ideas to fix

--- objects/exercise.js
+++ objects/exercise.js
@@ -4,12 +4,14 @@ console.log('inculcate: to teach and impress by frequent repetitions or admoniti
 
 // methods: start, stop reset
 
-function Stopwatch() {
-  let duration = 0;
+// Try using new Date() and getTime()
 
-  let incrementTime = function() {
-    duration += 1;
-  }
+function Stopwatch() {
+  let startTime, endTime, running, duration = 0;
+
+  // let incrementTime = function() {
+  //   duration += 1;
+  // }
 
   this.start = function() {
     setInterval(incrementTime, 1000);
@@ -25,9 +27,7 @@ function Stopwatch() {
   }
 
   Object.defineProperty(this, 'duration', {
-    get: function() {
-      return duration;
-    }
+    get: function() { return duration; }
   });
 }
 
