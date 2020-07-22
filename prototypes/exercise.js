4fc6aeffef02018473646e832cec718187341a8e
Jonathan Yale <jonathanyale@gmail.com>
2020-07-22 15:25:15 -0400

    attempt exercise. Code imcomplete

--- prototypes/exercise.js
+++ prototypes/exercise.js
@@ -1,25 +1,37 @@
+console.log('requisiteplay: needed for a particular purpose : essential, necessary');
+
 // Move instance members start, stop, reset to the prototype
 
 // You should put methods on the prototypes as an optimization technique
 
 // If you have a large number of objects of the same type, you don't want
-// multiple copies of these methods in memory. If you only have one instance
-// of the object (like here) you don't need to do this. Putting members on the 
-// prototype won't allow you to access them easily.
+// multiple copies of these methods in memory.
+
+// If you only have one instance of the object (like here) you don't need to do
+// this. Putting members on the prototype won't allow you to access them easily.
+
+// Premature optimization is the root of all evils.
 
 function Stopwatch() { 
   let startTime, endTime, running, duration = 0;
 
-  this.start = function() {
+  // Object.defineProperty()
+
+  Object.defineProperty(this, 'duration', {
+    get: function() { return duration; }
+  });
+}
+
+Stopwatch.prototype = {
+  start() {
     if (running) 
       throw new Error('Stopwatch has already started.');
-    
+
     running = true; 
 
     startTime = new Date();
-  };
-
-  this.stop = function() {
+  },
+  stop() {
     if (!running) 
       throw new Error('Stopwatch is not started.');
 
@@ -29,16 +41,13 @@ function Stopwatch() {
 
     const seconds = (endTime.getTime() - startTime.getTime()) / 1000;
     duration += seconds; 
-  };
-
-  this.reset = function() { 
+  },
+  reset() { 
     startTime = null;
     endTime = null;
     running = false; 
     duration = 0; 
-  };
-
-  Object.defineProperty(this, 'duration', {
-    get: function() { return duration; }
-  });
+  },
 }
+
+const sw = new Stopwatch();
